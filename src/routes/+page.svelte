085cb2b7f96f44ec264d4e727027dbe3aa7c54b2
Eduardo Liron <eduardoliron@gmail.com>
2025-10-06 08:55:42 -0300

    remove unused div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,3 @@
 
 <h1 class="text-4xl font-bold mb-8">Coming Soon</h1>
 <p>O nosso site está em construção. Fique atento!</p>
-
-<div class="social-media">
-	<a href="https://www.instagram.com/f.gardn/" target="_blank" class="fa fa-instagram"></a>
-</div>
